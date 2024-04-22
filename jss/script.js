72618cc7a89ca5bfe8a0fb5b191dde0545ef22f9
JulianAsnaw975 <167101244+JulianAsnaw975@users.noreply.github.com>
2024-04-22 23:59:37 +0700

    third commit

--- jss/script.js
+++ jss/script.js
@@ -1,1 +1,8 @@
 /* Ini File JS */
+function converter() {
+    let pertama = document.getElementById("celcius").value
+    
+    let hasil = ( parseInt(pertama) * 1.8 ) + 32 
+
+    console.log(hasil)
+}
