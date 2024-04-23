013269fb9d6b1d4b3f09c5287c76ad607b7a6498
JulianAsnaw975 <167101244+JulianAsnaw975@users.noreply.github.com>
2024-04-23 15:14:43 +0700

    fourth commit

--- jss/script.js
+++ jss/script.js
@@ -1,8 +1,22 @@
-/* Ini File JS */
-function converter() {
-    let pertama = document.getElementById("celcius").value
-    
-    let hasil = ( parseInt(pertama) * 1.8 ) + 32 
+document.getElementById('button-convert').onclick = tempConvert
+document.getElementById('button-reset').onclick = reset
 
-    console.log(hasil)
+function tempConvert(){
+    var celci = document.getElementById("celcius").value
+    var fahre = document.getElementById("fahrenheit").value
+    var kalkulasi = document.getElementById('kalkulasi').value
+    kalkulasi = 
+    hasil = ("* 1.8 + 32")
+
+    fahre = (parseInt(celci) * 1.8) + 32
+
+    document.getElementById('fahrenheit').value = parseInt(fahre)
+    document.getElementById('kalkulasi').value = hasil
+    return false
+}
+
+function reset() {
+    document.getElementById('celcius').value = ''
+    document.getElementById('fahrenheit').value = ''
+    document.getElementById('kalkulasi').value = ''
 }
