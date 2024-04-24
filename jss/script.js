878cce5ff8b8b2d98eca43db985dfb4c778131c5
JulianAsnaw975 <167101244+JulianAsnaw975@users.noreply.github.com>
2024-04-24 18:59:42 +0700

    sixth commit

--- jss/script.js
+++ jss/script.js
@@ -6,6 +6,11 @@ function tempConvert(){
     let celci = document.getElementById("celcius").value
     var fahre = document.getElementById("fahrenheit").value
     var kalkulasi = document.getElementById('kalkulasi').value 
+    let disFahreInp = document.querySelector('#fahrenheit')
+    let disFahreBtn = document.querySelector('#button-convert2')
+
+    disFahreInp.setAttribute('readonly', true)
+    disFahreBtn.setAttribute('disabled', true)
 
     let cToF = " * 1.8 + 32 "
 
@@ -16,7 +21,46 @@ function tempConvert(){
     fahre = (parseFloat(celci) * 1.8) + 32
 
     document.getElementById('fahrenheit').value = parseFloat(fahre).toFixed(2)
-    document.getElementById('kalkulasi').value = kalkulasi
+
+    if (celci != ""){
+        document.getElementById('kalkulasi').value = kalkulasi
+    } else {
+        document.getElementById('kalkulasi').value = ""
+        disFahreInp.removeAttribute('readonly')
+        disFahreBtn.removeAttribute('disabled')
+    }
+
+    return false
+}
+
+function secondConvert() {
+    var fahrenheit = document.getElementById("fahrenheit").value
+    let celcius = document.getElementById("celcius").value
+    var calculation = document.getElementById('kalkulasi').value
+    let disCelciInp = document.querySelector("#celcius")
+    let disCelciBtn = document.querySelector("#button-convert1")
+
+    disCelciInp.setAttribute('readonly', true)
+    disCelciBtn.setAttribute('disabled', true)
+
+    let fToC = " - 32 * 5/9 "
+    
+    let isi = ( parseFloat(fahrenheit) - 32 ) * 5/9 
+
+    calculation = fahrenheit + "°F" +  fToC + "=" + " " + parseFloat(isi).toFixed(2) + "°C"
+    
+    celcius = ( parseFloat(fahrenheit) - 32 ) * 5/9
+
+    document.getElementById('celcius').value = parseFloat(celcius).toFixed(2)
+
+    if (fahrenheit != ""){
+        document.getElementById('kalkulasi').value = calculation
+    } else {
+        document.getElementById('kalkulasi').value = ""
+        disCelciInp.removeAttribute('readonly')
+        disCelciBtn.removeAttribute('disabled')
+    }
+    
     return false
 }
 
@@ -24,22 +68,14 @@ function reset() {
     document.getElementById('celcius').value = ''
     document.getElementById('fahrenheit').value = ''
     document.getElementById('kalkulasi').value = ''
-}
+    let ActvFahreInp = document.querySelector('#fahrenheit')
+    let ActvCelciInp = document.querySelector("#celcius")
+    let ActvCelciBtn = document.querySelector("#button-convert1")
+    let ActvFahreBtn = document.querySelector("#button-convert2")
 
-function secondConvert() {
-    var fahrenheit = document.getElementById("fahrenheit").value
-    let celcius = document.getElementById("celcius").value
-    var calculation = document.getElementById('kalkulasi').value 
+    ActvFahreInp.removeAttribute('readonly')
+    ActvCelciInp.removeAttribute('readonly')
+    ActvCelciBtn.removeAttribute('disabled')
+    ActvFahreBtn.removeAttribute('disabled')
 
-    let fToC = " - 32 * 5/9 "
-
-    let isi = ( parseFloat(fahrenheit) - 32 ) * 5/9 
-
-    calculation = fahrenheit + "°F" +  fToC + "=" + " " + parseFloat(isi).toFixed(2) + "°C"
-
-    celcius = ( parseFloat(fahrenheit) - 32 ) * 5/9
-
-    document.getElementById('celcius').value = parseFloat(celcius).toFixed(2)
-    document.getElementById('kalkulasi').value = calculation
-    return false
 }
